ce9197075e8a395cf5c4ec19994d5e3ced582d7c
David Reese <david@reesedevelopment.com>
2021-12-12 10:01:47 +0200

    refinement

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from PIL import Image
+import subprocess
+import signal
 
 if len(sys.argv) >= 2:
     basepath = sys.argv[1]
@@ -11,26 +13,46 @@ else:
 
 directories.sort()
 
-def handle(path):
-    response = input(f"Delete \"{path}\"?").lower()
-    if response == "y":
-        print("Deleting...")
-        os.remove(path)
-        print("Deleted \"{dir}\"")
-    elif response == "v":
-        print('Showing file...')
-        name, extension = os.path.splitext(path)
-        if extension.lower() == ".png" or extension.lower() == ".jpg" or extension.lower() == ".jpeg":
-            with Image.open(path) as img:
-                img.show()
+def handle(path, completion=None):
+    try:
+        response = input(f"Delete \"{path}\"?\n").lower()
+        if response == "y":
+            print("Deleting...\n")
+            os.chmod(path, 0o777)
+            os.remove(path)
+            print(f"Deleted \"{path}\"\n")
+            if completion != None:
+                completion()
+        elif response == "v":
+            print('Showing file...\n')
+            name, extension = os.path.splitext(path)
+            if extension.lower() == ".png" or extension.lower() == ".jpg" or extension.lower() == ".jpeg":
+                with Image.open(path) as img:
+                    img.show()
+                    def c():
+                        if completion != None:
+                            completion()
+                        # img.hide()
+                    handle(path, c)
+            elif extension.lower() == ".pdf":
+                os.chmod(path, 0o777)
+                os.system(path)
+                # subprocess.Popen([path], shell=True)
+                # plot = subprocess.Popen("evince '%s'" % path, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+                handle(path)
+            else:
+                print("Can't show this file.\n")
+                # with open(path) as infile:
+                #     for line in infile:
+                #         outfile.write(line)
+                handle(path)
         else:
-            print("Can't show this file.")
-            # with open(path) as infile:
-            #     for line in infile:
-            #         outfile.write(line)
+            print("Skipped file.\n")
+            if completion != None:
+                completion()
+    except PermissionError:
+        print(f"A PermissionError occured.\n")
         handle(path)
-    else:
-        print("Skipped file.")
 
 for path in directories:
     if basepath == None:
